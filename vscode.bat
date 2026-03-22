67f2e9d6df07f373ae62f9f13e0145f0245e7545
sedcftyv <49577634+sedcftyv@users.noreply.github.com>
2026-03-23 00:41:08 +0800

    x64 native compiler

--- vscode.bat
+++ vscode.bat
@@ -1,4 +1,5 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+::call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cd c://vscode-cpp
 code .
 exit
